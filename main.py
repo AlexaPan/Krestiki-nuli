b348759bd587efce3c75509940b90a82137bf4d7
AlexaPan <xvspan@gmail.com>
2025-01-21 18:53:57 +0300

    changed code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 
 game_win = tk.Tk()
 game_win.title("The Game 'Krestiki'")
-game_win.geometry("300x300")
+game_win.geometry("300x400")
 game_win.resizable(False, False)
 
 buttons = []
@@ -15,17 +15,22 @@ def win_creation():
     for i in range(3):
         row_buttons = []
         for j in range(3):
-            but = tk.Button(game_win, text="", width=5, height=2, font = ("Arial", 20),
+            but = tk.Button(game_win, text="", width=5, height=2, font = ("Arial", 10),
                                     command=lambda row_w=i, col_w=j: button_click(row_w, col_w))
             row_buttons.append(but)
             but.grid(row = i, column = j)
         buttons.append(row_buttons)
 
+    new_game_but = tk.Button(game_win, text = "New Game", width=5, height=2, font = ("Arial", 10),
+                             command = lambda: new_game())
+    new_game_but.grid(row = 5, column = 2)
 
 
+def new_game():
+    pass
 
 
-def button_click(i, j):
+def button_click(row, col):
     pass
 
 win_creation()
